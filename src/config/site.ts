1d572c50388c140d9822214ca69e4e77a31b5e87
Condro Wiyono <condro@outlook.co.id>
2024-03-15 23:38:31 +0700

    chore: init project

---
+++ src/config/site.ts
@@ -0,0 +1,62 @@
+export type Site = typeof siteConfig;
+
+export const siteConfig = {
+  hero: {
+    heading: "Callender Hamilton (CH)",
+    subHeading:
+      "Proyek KPBU Penggantian atau Duplikasi jembatan Callender Hamilton (CH) di Pulau Jawa oleh PT Baja Titian Utama",
+    image1: "/hero/about.webp",
+    image2: "/hero/about.webp",
+    image3: "/hero/about.webp",
+  },
+  activity: {
+    heading: "KBPU",
+    subHeading: "Kerjasama Pemerintah dan Badan Usaha",
+    data: [
+      {
+        title: "12 th",
+        description: "Masa Konsesi",
+      },
+      {
+        title: "37",
+        description: "Jembatan",
+      },
+      {
+        title: "DBFOM",
+        description: "Design, Build, Finance, Operate, Maintain, Transfer",
+      },
+      {
+        title: "AP",
+        description: "(Availability Payment)",
+      },
+    ],
+  },
+  menus: [
+    {
+      name: "Beranda",
+      url: "/#home",
+    },
+    {
+      name: "Profil",
+      url: "#profile",
+    },
+    {
+      name: "Aktivitas",
+      url: "#activities",
+    },
+    {
+      name: "Galeri",
+      url: "#gallery",
+    },
+    {
+      name: "Instagram",
+      url: "https://www.instagram.com/bajatitianutama.official",
+      external: true,
+    },
+    {
+      name: "Hubungi Kami",
+      url: "#contact",
+      text: "primary",
+    },
+  ],
+};
